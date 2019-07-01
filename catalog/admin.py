4a821a8ffa43af61ee2263b190aa31822a9cee07
TamoshaytisV <amd.vladas@gmail.com>
2019-07-01 20:26:17 +0300

    refactoring

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,7 +5,7 @@ from .models import Category, Pizza
 class PizzaAdmin(admin.ModelAdmin):
     list_display = ('name', 'price_small', 'price_large')
     fields = ('name', 'description', 'price_small', 'price_large', 'category', 'photo')
-    list_filter = ('category__type',)
+    list_filter = ('category__name',)
 
 
 admin.site.register(Category)
