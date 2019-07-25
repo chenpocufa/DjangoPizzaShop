9e0e64a25c6408398d5e96ed35daed3e19060a7c
Vladas Tamoshaitis <amd.vladas@gmail.com>
2019-07-25 15:09:10 +0300

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,7 +5,7 @@ from django import forms
 from django.utils.translation import gettext_lazy as _
 
 from accounts.models import User
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm, ReadOnlyPasswordHashField
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 
 class UserAdminCreationForm(UserCreationForm):
@@ -27,15 +27,6 @@ class UserAdminChangeForm(UserChangeForm):
 
     Includes all the fields on the user, but replaces the password field with admin's password hash display field.
     """
-    password = ReadOnlyPasswordHashField(
-        label=_("Password"),
-        help_text=_(
-            "Raw passwords are not stored, so there is no way to see this "
-            "user's password, but you can change the password using "
-            "<a href=\"{}\">this form</a>."
-        ),
-    )
-
     class Meta:
         model = User
         fields = '__all__'
