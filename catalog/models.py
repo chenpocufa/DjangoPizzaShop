3f34d296160908f75d0569f2642747b018836b26
TamoshaytisV <amd.vladas@gmail.com>
2019-08-14 22:57:40 +0300

    add translations

--- catalog/models.py
+++ catalog/models.py
@@ -2,32 +2,34 @@
 Catalogue models.
 """
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class Category(models.Model):
     """
     Category model.
     """
-    name = models.CharField(
-        max_length=100,
-    )
+    name = models.CharField(verbose_name=_('Name'), max_length=100)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name_plural = 'Categories'
+        verbose_name_plural = _('Categories')
 
 
 class Pizza(models.Model):
     """
     Pizza model.
     """
-    name = models.CharField(max_length=100)
-    content = models.CharField(max_length=100)
-    description = models.TextField(max_length=300)
-    category = models.ManyToManyField(Category)
-    photo = models.ImageField(upload_to='images/')
+    name = models.CharField(verbose_name=_('Name'), max_length=100)
+    content = models.CharField(verbose_name=_('Content'), max_length=100)
+    description = models.TextField(verbose_name=_('Description'))
+    category = models.ManyToManyField(Category, verbose_name=_('Category'))
+    photo = models.ImageField(upload_to='images/', verbose_name=_('Image'))
+
+    class Meta:
+        verbose_name_plural = _("Pizzas")
 
     @property
     def categories_display(self):
@@ -48,13 +50,13 @@ class Pizza(models.Model):
 
 class Size(models.Model):
     CHOICES = (
-        ('small', 'Small'),
-        ('large', 'Large'),
+        ('small', _('Small')),
+        ('large', _('Large')),
     )
-    type = models.CharField(max_length=20, choices=CHOICES)
-    price = models.DecimalField(default=0, max_digits=5, decimal_places=2)
-    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE, related_name='sizes')
-    active = models.BooleanField(default=False,)
+    type = models.CharField(max_length=20, choices=CHOICES, verbose_name=_('Type'))
+    price = models.DecimalField(default=0, max_digits=5, decimal_places=2, verbose_name=_('Price'))
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE, related_name='sizes', verbose_name=_('Pizza'))
+    active = models.BooleanField(default=False, verbose_name=_('Active'))
 
     def __str__(self):
         return f"Size"
