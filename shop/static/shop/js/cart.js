d84e99d662b849f4516aa2cf43486af94a7b78f3
Benedikt89 <constantine.kostka@gmail.com>
2019-09-03 06:56:16 +0300

    Finished-Cart

--- shop/static/shop/js/cart.js
+++ shop/static/shop/js/cart.js
@@ -46,8 +46,8 @@ var shoppingCart = (function() {
         for(var item in cart) {
             if(cart[item].id === id && cart[item].size === size) {
                 cart[item].quantity --;
-                if(cart[item].quantity === 0) {
-                    cart.splice(item, 1);
+                if(cart[item].quantity <= 1) {
+                    cart[item].quantity = 1;
                 }
                 break;
             }
@@ -107,17 +107,6 @@ var shoppingCart = (function() {
         return cartCopy;
     }
 
-    // cart : Array
-    // Item : Object/Class
-    // addItemToCart : Function
-    // removeItemFromCart : Function
-    // removeItemFromCartAll : Function
-    // clearCart : Function
-    // countCart : Function
-    // totalCart : Function
-    // listCart : Function
-    // saveCart : Function
-    // loadCart : Function
     return obj;
 })();
 
@@ -126,15 +115,15 @@ function displayCart() {
     var output = "";
     for(var i in cartArray) {
         output += "<tr class='cart-row'>"
-            + "<td class='align-middle'><img class=\"rounded mx-auto d-block small-Img\" src=" + cartArray[i].image + "></td>"
+            + "<td class='align-middle'><div class=\"small-Img\"><img src=" + cartArray[i].image + "></div></td>"
             + "<td class='align-middle'><div><h5>" + cartArray[i].name + "</h5><p class='font-italic'>" + cartArray[i].size + "</p></div></td>"
             + "<td class='align-middle'>" + cartArray[i].price + "</td>"
-            + "<td class='align-middle'><div class='input-group'><span class='minus-item btn btn-small font-weight-bold'" +
+            + "<td class='align-middle'><div class='input-group'><span class='minus-item font-weight-bold'" +
             " data-id=" + cartArray[i].id + " data-size=" + cart[i].size + ">-</span>"
             + "<span type='number' " +
             "class='item-count font-weight-bold' data-id='" + cartArray[i].id + "'>" + cartArray[i].quantity + "</span>"
             + "<span " +
-            "class='plus-item btn btn-small font-weight-bold' data-id=" + cartArray[i].id + "" +
+            "class='plus-item font-weight-bold' data-id=" + cartArray[i].id + "" +
             " data-size=" + cartArray[i].size + ">+</span></div></td>"
             + "<td class='align-middle font-weight-bold'>" + cartArray[i].total + "</td>"
             + "<td class='align-middle byn-text'>\ BYN \</td>"
