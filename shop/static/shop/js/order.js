f3a4dca3eee9ba52986a58c865effb33658e5f7f
Benedikt89 <constantine.kostka@gmail.com>
2019-09-03 23:12:31 +0300

    js-works, but ome mistake on jango а

--- shop/static/shop/js/order.js
+++ shop/static/shop/js/order.js
@@ -21,29 +21,37 @@ $(document).ready(function(){
 //    });
 //});
 
-// let submitBtn = document.getElementById('order-submit');
-// submitBtn.addEventListener('click', );
+//     let submitBtn = document.getElementsByClassName('order-submit');
+//     submitBtn.addEventListener('click', {function(event) {
+//        event.preventDefault();
+//        let order = localStorage.getItem('order');
+//        let form = new FormData(document.querySelector("#order-form"));
+//        form.append("order", order);
+//        postData("/order/", form);
+//     }});
 
 
 function addThisOrder (){
-
     let order = localStorage.getItem('order');
-    let form = new FormData(document.getElementById("order-form"));
+    let form = new FormData(document.querySelector("#order-form"));
+    alert(form);
     form.append("order", order);
+    debugger;
     postData("/order/", form);
 };
+//  'id': 'phone', , 'id': 'text', 'id': 'time', 'id': 'address', 'id': 'comment'
 
 function postData(url = '', data = {}) {
-
+    alert(data);
     return fetch(url, {
         method: 'POST',
         redirect: 'manual',
         body: data,
     })
+
         .then(localStorage.clear())
         .then(response => {window.location="/"});
 }
-
 
 
 
