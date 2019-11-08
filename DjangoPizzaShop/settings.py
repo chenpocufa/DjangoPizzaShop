ed396b2d965a0a9fd2b4fb8284b9416e2a1da4ed
Ice Ivanov <ice.ivanov@gmail.com>
2019-11-08 16:56:42 +0300

    changed allowed hosts

--- DjangoPizzaShop/settings.py
+++ DjangoPizzaShop/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', False)
 
-ALLOWED_HOSTS = [os.environ.get('HOST_NAME', 'localhost'), os.environ.get('HOST_IP', '127.0.0.1')]
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = [os.environ.get('HOST_NAME', 'localhost'), os.environ.get('HOST_IP', '127.0.0.1')]
 
 # Application definition
 
